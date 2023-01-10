f6a805862f2fd9500c6e8e062d996c2272e309cc
minuxdev <minux.midi@gmail.com>
2023-01-10 23:42:12 +0200

    Added media controllers packages

--- scripts/pos_installation.sh
+++ scripts/pos_installation.sh
@@ -4,7 +4,7 @@
 #   INSTALLING YAY-GIT
 function yay_installation() {
 
-  printf"
+  printf "
 
     INSTALLING YAY-GIT 
 
@@ -27,7 +27,7 @@ function yay_installation() {
 
 function terminal_emulator() {
 
-  printf"
+  printf "
 
     INSTALLING TERMINAL_EMULATOR + LVIM
 
@@ -36,15 +36,15 @@ function terminal_emulator() {
     FONTS_DIR="$HOME/.fonts/"
     sudo pacman -Sy zsh alacritty ranger dialog --noconfirm
 
-    cp -r "$HOME/dotfiles/term_emulator/alacritty" "$HOME/.config/"
+    cp -rv "$HOME/dotfiles/term_emulator/alacritty" "$HOME/.config/"
     
     # installing oh-my-zsh
-    [ ! -d "$HOME/.oh-my-zsh" ] && rm -r "$HOME/.oh-my-zsh";
+    [ ! -d "$HOME/.oh-my-zsh" ] && rm -rv "$HOME/.oh-my-zsh";
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     # installing p10k themes + nerd fonts
     [ ! -d "$FONTS_DIR" ] && mkdir "$FONTS_DIR" || printf "Directory found! Installing fonts.\n\n";
-    cp -rv "$HOME/dotfiles/fonts/SpaceMono" "$FONTS_DIR"
+    cp -rvv "$HOME/dotfiles/fonts/SpaceMono" "$FONTS_DIR"
     fc-cache
     fc-list | awk ' /Space/ '
     sleep 2s
@@ -63,9 +63,9 @@ function terminal_emulator() {
 
     if [ -f "$HOME/.local/bin/lvim" ]
     then
-        awk -i inplace 'NR==1 { print "\nexport PATH=$HOME/.local/bin:$PATH\nalias vim=lvim" }; 1' ~/.zshrc
+        awk -i inplace 'NR==1 { print "\n\nexport PATH=$HOME/.local/bin:$PATH\n\nalias vim=lvim" }; 1' ~/.zshrc
 	      source ~/.zshrc
-        echo -e "\nLunarVim installed successfully. Open it to complete the process...\n"
+        echo -e "\n\nLunarVim installed successfully. Open it to complete the process...\n"
     else
         echo "Lvim not found!!!" 
     fi
@@ -73,6 +73,12 @@ function terminal_emulator() {
 
 
 function install_hyprland() {
+    printf "
+          
+        HYPRLAND INSTALLATION 
+
+    "
+
     # installing xorg-xwaylnad
     HYPR_DIR="$HOME/.config/hypr/"
     sudo pacman -S xorg-xwayland --noconfirm
@@ -80,41 +86,56 @@ function install_hyprland() {
   # install and config DE
     yay -S hyprland-git waybar-git hyprpaper-git wofi --noconfirm
     [ ! -d "$HYPR_DIR" ] && mkdir "$HYPR_DIR"
-    cp -r "$HOME/dotfiles/hyprland/hypr/" "$HYPR_DIR"
-    cp -r "$HOME/dotfiles/waybar/" "$HOME/.config/"
+    cp -rv "$HOME/dotfiles/DE/hyprland/hypr/" "$HYPR_DIR"
+    cp -rv "$HOME/dotfiles/DE/hyprland/waybar" "$HOME/.config/"
     echo -e "\nHyprland installed successfully!!"
 }
 
 function install_sddm() {
+  printf "
+      INSTALLING SDDM + FLOWER-THEME
 
+  "
     SDDM="$HOME/dotfiles/DM/sddm/sddm.conf"
     SDDM_THEMES_SRC="$HOME/dotfiles/DM/sddm/sddm-themes/"
     SDDM_THEMES_DST="/usr/share/sddm/themes/"
 
+    yay -S sddm-git --noconfirm
+    
     awk -i inplace -v cu="$(whoami)" ' /User=/ { sub(/User=/, "&"cu) }; 1 ' "$SDDM"
     awk -i inplace -v cu="sddm-flower-theme" ' /Current=/ { sub(/Current=/, "&"cu) }; 1 ' "$SDDM"
     
-    yay -S sddm-git --noconfirm
-    
-    sudo cp -r "$SDDM" "/etc/"
-    sudo cp -r "$SDDM_THEMES_SRC" "$SDDM_THEMES_DST"
+    sudo cp -rv "$SDDM" "/etc/"
+    sudo cp -rv "$SDDM_THEMES_SRC" "$SDDM_THEMES_DST"
 
     sudo systemctl enable sddm now
+
+    printf "
+    
+    All set to run sddm.
+
+    "
 }
 
 function players() {
+  printf "
+
+      INSTALLING MEDIA CONTROLLERS
+
+  "
+    sudo pacman -S --noconfirm pulseaudio pulseaudio-alsa pavucontrol
     sudo pacman -S mpv cmus
 }
 
 function utilities() {
-    sudo pacman -S unzip jmtpfs --noconfirm
+    sudo pacman -S dolphin unzip jmtpfs --noconfirm
     yay -Sy gvfs-mtp --noconfirm
 }
 
 
-#yay_installation
+yay_installation
 terminal_emulator
-#install_hyprland
-#install_sddm
-#players
-#utilities
+install_hyprland
+install_sddm
+players
+utilities
