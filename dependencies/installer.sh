f54bbf4d71e52e72b0c7b953e7b8521f8132a53a
minuxdev <minux.midi@gmail.com>
2023-07-20 13:06:39 +0200

    Merged functions in dependencies to installer.sh

---
+++ dependencies/installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+installer() {
+  YAY_DIR="$HOME/.git-clones"
+  [ ! -d "$YAY_DIR" ] && mkdir "$YAY_DIR"
+  cd "$YAY_DIR" && 
+  [ ! -f "/usr/bin/git" ] && sudo pacman -Sy git --noconfirm &&
+  git clone https://aur.archlinux.org/yay-git.git &&
+  cd "yay-git" &&
+  makepkg -si
+}
+
+audio ()
+{
+  sudo pacman -Sy \
+    pulseaudio pulseaudio-ctl pulseaudio-alsa \
+    pulseaudio-bluetooth pavucontrol playerctl
+}
+
+others ()
+{
+  sudo pacman -Sy ripgrep exa fzf --noconfirm
+}
+
+installer &&
+audio &&
+others
