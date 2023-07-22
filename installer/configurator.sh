7dfd57317f0c6b6f895ee0236cdc1eff7684f814
minuxdev <minux.midi@gmail.com>
2023-07-22 02:49:13 +0200

    Enabled ParallelDownloads in pacman.conf

--- installer/configurator.sh
+++ installer/configurator.sh
@@ -25,27 +25,29 @@ locale-gen
 printf "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # network configuration
-printf "\nNetwork configuration\n"
-printf "archminux" > /etc/hostname
+read -p "Insert hostname: " HOSTNAME
+printf "$HOSTNAME" > /etc/hostname
 
 # install grub
 printf"\n\nInstalling grub\n\n"
 grub-install --efi-directory=/efi --target=x86_64-efi 
-[ $? = 0 ] || echo "GRUB-INSTALL FAILED TO INSTALL TRY GAIN!";
+[[ $? = 0 ]] || echo "GRUB-INSTALL FAILED TO INSTALL TRY GAIN!";
 grub-mkconfig -o /boot/grub/grub.cfg
 efibootmgr
+
+# Enabling parallel downloads
+awk -i inplace ' /ParallelDownloads/ { $1 = substr($1,2) }; 1 ' /etc/pacman.conf
 
 # setting roots password
 printf "\nSET A PASSWORD FOR USER ROOT\n"
 passwd
 
-printf """
+printf """\n
 CONGRATULATIONS!!!
 
-THE INSTALLATION FINISHED SUCCESSFULLY. 
-EXECUTE THE pos_installation.sh SCRIPT SO FURTHER CONFIGURATIONS CAN BE MADE.
+THE INSTALLATION HAS FINISHED SUCCESSFULLY. 
 
-PLEASE REBOOT...
+EXITING FROM CHROOT...
 """
 sleep 3s
 exit;
