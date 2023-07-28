4630d08d8ffa3b3128475fbaf1070db93d1a8c17
minuxdev <minux.midi@gmail.com>
2023-07-29 01:30:32 +0200

    Updated from networking branch.

--- networking/network.d
+++ networking/network.d
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 network() {
-  sudo systemctl enable --now NetworkManager
-  sudo cp -r ~/dotfiles/networking/system-connections /etc/NetworkManager/
+  if [ -f "/usr/bin/nmcli" ]
+  then 
+    sudo systemctl enable --now NetworkManager
+    sudo cp -r ~/dotfiles/networking/system-connections /etc/NetworkManager/
+  else 
+    prinf "NetworkManager isn't installed! Make sure you install it first!"
+  fi
 }
 
 network
