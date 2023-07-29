485b8c548fed0ce49259971d1dc787e764b1db6f
minuxdev <minux.midi@gmail.com>
2023-07-30 00:52:47 +0200

    Updated ranger/rc.config file.

--- file_managers/installer.sh
+++ file_managers/installer.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/zsh
+#
 BASE_DIR="$HOME/dotfiles/file_managers"
 
 ranger () {
   sudo pacman -S ranger ueberzug highlight --noconfirm
-  yay -S poppler --noconfirm
+  yay -S bat poppler --noconfirm
   cp -rv "$BASE_DIR/ranger" "$HOME/.config/"
+
+  VALUES=('EDITOR=nvim' 'BAT_STYLE=base16' 'SHELL=/usr/bin/zsh')
+  for exported in "${VALUES[@]}"
+  do 
+    sed -i " /EXPORTS/a\export $exported " ~/.zshrc
+  done
+  source ~/.zshrc
 }
 
 pacman -S nautilus --noconfirm
