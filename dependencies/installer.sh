fe760cd3cfda7f92fb4da29a46581881b8b491d3
minuxdev <minux.midi@gmail.com>
2023-07-30 12:25:16 +0200

    Removed NVM from dependencies.

--- dependencies/installer.sh
+++ dependencies/installer.sh
@@ -17,13 +17,6 @@ audio ()
     pulseaudio-bluetooth pavucontrol playerctl
 }
 
-nvm () {
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash 
-  touch "$HOME/.zshrc"
-  echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" ' > "$HOME/.zshrc"
-}
-
 others ()
 {
   sudo pacman -Sy ripgrep --noconfirm
@@ -32,4 +25,3 @@ others ()
 installer 
 audio 
 others
-nvm
