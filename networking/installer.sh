0ac6fcc8c04d621902d0c8080afbf12a9327e068
minuxdev <minux.midi@gmail.com>
2023-12-03 14:22:19 -0800

    Switched from NetworkManager to systemd-networkd

--- networking/installer.sh
+++ networking/installer.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
+BASE_DIR="$HOME/dotfiles/networking"
 
 network() {
-  sudo systemctl enable --now NetworkManager
-  sudo cp -r ~/dotfiles/networking/system-connections /etc/NetworkManager/
+  sudo systemctl enable --now systemd-networkd
+  sudo cp -r $BASE_DIR/configs/network /etc/systemd/
+  sudo cp -r $BASE_DIR/configs/resolv.conf /etc/
+  ping -c 4 google.com
+}
+
+_samba() {
+  sudo pacman -Sy samba --noconfirm
+  sudo smbpasswd -a $USER
+
+  sudo cp -r $BASE_DIR/samba /etc/
+  sudo systemctl enable --now smb
 }
 
 network
+_samba
--- programming/installer.sh
+++ programming/installer.sh
@@ -6,69 +6,69 @@ source "$HOME/dotfiles/progress_notes.sh"
 
 npm_packages () 
 { 
-  	start_task 'NPM PACKAGES'
+  start_task 'NPM PACKAGES'
 
-  	sudo npm install -g live-server 
-  	sudo npm install -g sass
+  sudo npm install -g live-server 
+  sudo npm install -g sass
 
-  	$set_aliases 'lvs="live-server $1"'
+  $set_aliases 'lvs="live-server $1"'
 
-  	end_task
+  end_task
 }
 
 text_editors () 
 {
-  	start_task 'TEXT EDITORS'
-  
-  	sudo pacman -S kate --noconfirm
-  	[ $? != 0 ] && sudo pacman -S gedit --noconfirm
+  start_task 'TEXT EDITORS'
+
+  sudo pacman -S kate --noconfirm
+  [ $? != 0 ] && sudo pacman -S gedit --noconfirm
 
 	end_task
 }
 
 nvim () 
 {
-  	start_task 'IDE: NVIM'
-  
-    packages='python-pytest python-neovim htmlhint ctags'
+  start_task 'IDE: NVIM'
 
-  	sudo pacman -S neovim
-    sudo pacman -S $packages --noconfirm
-  	yay -S wl-clipboard flake8 --noconfirm
+  packages='python-pytest python-neovim htmlhint ctags'
 
-	    CONFIG_DIR="$HOME/.config/nvim"
-    [ ! -d "$CONFIG_DIR" ] && "$CONFIG_DIR" "$CONFIG_DIR.BKP"
-  	cp -rv "$BASE_DIR/nvim" "$CONFIG_DIR"
-  	$set_aliases 'nv="nvim"'
+  sudo pacman -S neovim
+  sudo pacman -S $packages --noconfirm
+  yay -S wl-clipboard flake8 --noconfirm
+
+    CONFIG_DIR="$HOME/.config/nvim"
+  [ ! -d "$CONFIG_DIR" ] && "$CONFIG_DIR" "$CONFIG_DIR.BKP"
+  cp -rv "$BASE_DIR/nvim" "$CONFIG_DIR"
+  $set_aliases 'nv="nvim"'
   
 	end_task
 }
 
 _docker () {
-  	start_task 'DOCKER AND DOCKER DESKTOP'
-    
-    # Install docker 
-    #sudo pacman -S docker --noconfirm && sudo usermod -aG docker $USER 
+  start_task 'DOCKER AND DOCKER DESKTOP'
+  
+  # Install docker 
+  sudo pacman -S docker --noconfirm && sudo usermod -aG docker $USER 
 
-sudo curl -L \
+  sudo curl -L \
   "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
   -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 
-    ln -s /usr/lib/libcrypt.so /usr/lib/libcrypt.so.1
+  ln -s /usr/lib/libcrypt.so /usr/lib/libcrypt.so.1
 
-    printf "Docker Version: `docker -v`\ndocker-compose version: `docker-compose --version`"
-    printf '%c' "Docker and docker-compose installed successfully. You need to reboot the system."
-    $set_aliases 'doc="docker-compose"'
+  printf "Docker Version: `docker -v`\ndocker-compose version: `docker-compose --version`"
+  printf '%c' "Docker and docker-compose installed successfully. You need to reboot the system."
+  $set_aliases 'doc="docker-compose"'
 
 	end_task
 }
 
 _staruml () {
-  	start_task 'STARUML'
+  start_task 'STARUML'
 
-    yay -S staruml --noconfirm
-  
-    end_task
+  yay -S staruml --noconfirm
+
+  end_task
 }
 
 _postgresql () {
@@ -99,17 +99,17 @@ _staruml
 _postgresql
 
 ALIASES=(
-   'da="python manage.py"'
-   'dac="python manage.py createsuperuser "'
-   'dar="python manage.py runserver"'
-   'das="python manage.py startapp"'
-   'dat="python manage.py test"'
-   'gs="git status"'
-   'gl="git log --oneline"'
-   'ga="git add"'
-   'gc="git commit -m"'
-   'gub="git checkout $1 -- $2"'
-   'uml="staruml"'
+  'da="python manage.py"'
+  'dac="python manage.py createsuperuser "'
+  'dar="python manage.py runserver"'
+  'das="python manage.py startapp"'
+  'dat="python manage.py test"'
+  'gs="git status"'
+  'gl="git log --oneline"'
+  'ga="git add"'
+  'gc="git commit -m"'
+  'gub="git checkout $1 -- $2"'
+  'uml="staruml"'
 )
 
 $set_aliases "${ALIASES[@]}"
