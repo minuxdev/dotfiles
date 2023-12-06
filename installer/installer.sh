ab1b7ac868de91b62dae49754cee1557599f1076
minuxdev <minux.midi@gmail.com>
2023-12-06 12:04:37 -0800

    Added linux-firmware on the list.

--- installer/installer.sh
+++ installer/installer.sh
@@ -103,7 +103,7 @@ e.g. nvme0n1p1
 
     # install system
     printf "\n\nBeginning the system installation\n"
-    pacstrap /mnt base-devel linux systemd-sysvcompat iputils git grub efibootmgr ntfs-3g --noconfirm
+    pacstrap /mnt base-devel linux linux-firmware systemd-sysvcompat iputils git grub efibootmgr ntfs-3g --noconfirm
 
     [ $? = 0 ] || ( echo "SORRY! THE INSTALLATION FAILED!"; exit 13; )
 
