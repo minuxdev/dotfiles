8860bec76da58b45fa5219752bcdb3882b6f2214
minuxdev <minux.midi@gmail.com>
2023-12-07 00:24:45 +0200

    Switched to from wofi to rofi.

--- dewm/installer.sh
+++ dewm/installer.sh
@@ -5,66 +5,68 @@ source "$HOME/dotfiles/progress_notes.sh"
 CONFIG_DIR="$HOME/.config"
 BASE_SRC_DIR="$HOME/dotfiles/dewm"
 
-hyprland ()
-{
-  start_task 'HYPRLAND'
+hyprland() {
+	start_task 'HYPRLAND'
 
-  sudo pacman -Sy hyprland --noconfirm
-  cp -rv "$BASE_SRC_DIR/hypr" "$CONFIG_DIR/"
+	sudo pacman -Sy hyprland --noconfirm
+	cp -rv "$BASE_SRC_DIR/hypr" "$CONFIG_DIR/"
 
-  end_task 
+	end_task
 }
 
-hyprpaper ()
-{
-  start_task 'HYPRPAPER'
+hyprpaper() {
+	start_task 'HYPRPAPER'
 
-  yay -S hyprpaper-git --noconfirm
+	yay -S hyprpaper-git --noconfirm
 
-  end_task 
+	end_task
 }
 
-waybar ()
-{
-  start_task 'WAYBAR'
-  
-  sudo pacman -S waybar --noconfirm
-  cp -rv "$BASE_SRC_DIR/waybar" "$CONFIG_DIR/"
+waybar() {
+	start_task 'WAYBAR'
 
-  end_task 
+	sudo pacman -S waybar --noconfirm
+	cp -rv "$BASE_SRC_DIR/waybar" "$CONFIG_DIR/"
+
+	end_task
 }
 
+_rofi() {
+	start_task 'ROFI'
 
+	yay -S rofi --noconfirm
+	cp -rv "$BASE_SRC_DIR/rofi" "$CONFIG_DIR/"
 
-wofi ()
-{
-  start_task 'WOFI'
-
-  yay -S wofi --noconfirm
-  cp -rv "$BASE_SRC_DIR/wofi" "$CONFIG_DIR/"
-
-  end_task
+	end_task
 }
 
-_sddm () 
-{
-  start_task 'SDDM'
-  
-  yay -S sddm-git --noconfirm
+_sddm() {
+	start_task 'SDDM'
 
-  [ ! -f /etc/sddm.conf ] &&
-    sudo echo "
+	sudo pacman -S sddm --noconfirm
+
+	[ ! -f /etc/sddm.conf ] &&
+		sudo echo "
 [Autologin]
 User=$USER
 Session=hyprland
-      " > /etc/sddm.conf ||
-        sudo sed -i -e "s/User=/User=$USER/" -e "s/Session=/Session=hyprland" /etc/sddm.conf
+      " >/etc/sddm.conf ||
+		sudo sed -i -e "s/User=/User=$USER/" -e "s/Session=/Session=hyprland" /etc/sddm.conf
 
-  systemctl enable sddm
+	systemctl enable sddm
 
-  end_task
+	end_task
 }
 
-[ ! -f "/usr/bin/yay" ] && 
-  (printf "Helper not installed!"; exit 10) || 
-  (hyprland; hyprpaper; waybar; wofi; _sddm)
+[ ! -f "/usr/bin/yay" ] &&
+	(
+		printf "Helper not installed!"
+		exit 10
+	) ||
+	(
+		hyprland
+		hyprpaper
+		waybar
+		_rofi
+		_sddm
+	)
